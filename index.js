08fa98ac5387c8bbbd92b79eb680a9fa963f54ba
Simon Lee <simonhlee97@hotmail.com>
2020-09-01 11:04:53 +0900

    patch to 1.0.1

--- index.js
+++ index.js
@@ -1,31 +1,3 @@
-function log(msg) {
-	console.log(msg);
-};
-
-function shout(msg) {
-	if (typeof msg === str) {
-		console.log(msg.toUpperCase())
-	} else {
-		log(msg)
-	};
-};
-
-function whisper(msg) {
-	if (typeof msg === str) {
-		console.log(msg.toLowerCase())
-	} else {
-		log(msg)
-	};
-};
-
-module.exports = {log, shout, whisper}
-
-// later, import the functions using destructuring
-// const { log, shout, whisper } = require('mynpm');
-
-
-
-
 function log(msg, options = { loudness: 'default' }) {
 	if (options && options.loudness) {
 		switch (options.loudness) {
@@ -41,6 +13,6 @@ function log(msg, options = { loudness: 'default' }) {
 	}
 }; module.exports = log;
 
-// user will import using optional arg
-const log = require('my-module-name');
-log('Hello World', { loudness: 'shout' });
+// example usage: import using optional arg
+// const log = require('mynpm');
+// log('Hello World', { loudness: 'shout' });
